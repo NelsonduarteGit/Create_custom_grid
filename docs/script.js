b3476743ad55caa4c735347da12626ef2f995154
Nelson Duarte <88665961+NelsonduarteGit@users.noreply.github.com>
2021-10-24 14:08:12 +0100

    Added ex01

--- docs/script.js
+++ docs/script.js
@@ -49,3 +49,66 @@ function rush00() {
     console.log(grid);
     resposta.innerHTML = grid;
 }
+
+
+// Segundo exercicio do Rush00
+function rush01() {
+
+    let x = parseInt(document.getElementById("row1").value);
+    let y = parseInt(document.getElementById("col1").value);
+    let resposta = document.getElementById("mostrar_grid1");
+    let grid1 = "";
+
+    var tf = "/";
+    var tt = "\\";
+    var estrela = "*";
+    var espaço = "&nbsp";
+
+    var a = 0;
+    var b = 0;
+
+    while (a < y)
+    {
+        b = 0;
+        while (b < x)
+        {
+            // 1/
+            if (a == 0 && b == 0) 
+                grid1 = grid1 + tf;             
+            
+            // 2\
+            else if (a == 0 && b == (x - 1))
+                grid1 = grid1 + tt + "<br />"; 
+            
+            // 3\
+            else if (a == (y - 1) && b == 0) 
+                grid1 = grid1 + tt;
+            
+            // 4/
+            else if (a == (y - 1) && b == (x - 1))         
+                grid1 = grid1 + tf + "<br />";
+
+            // * de cima e baixo
+            else if ((a == 0 && b > 0 && b != (x - 1))                
+            || (a == (y - 1) && b > 0 && b != (x - 1)))     
+                grid1 = grid1 + estrela;
+
+            // * da esquerda
+            else if (a > 0 && b == 0)                              
+                grid1 = grid1 + estrela;
+            
+            // * da direita
+            else if (a > 0 && b == (x - 1))
+                grid1 = grid1 + estrela + "<br />";
+        
+            // espaço no meio
+            else
+                grid1 = grid1 + espaço;
+            b++;
+        }
+        a++;
+    }
+    console.log(grid1);
+    resposta.innerHTML = grid1;
+}
+
